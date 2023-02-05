3cdce7985d671e7c117690134b49d1e2a6db09e2
alnajm01 <mohab.alnajjar@pfizer.com>
2023-02-05 11:14:39 +0200

    to 6

--- Back-end/TeamUtility.Api/Program.cs
+++ Back-end/TeamUtility.Api/Program.cs
@@ -6,10 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.RegisterServices();
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
+}
 
 var app = builder.Build();
 
@@ -19,15 +22,16 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseCors(_cors => _cors.WithOrigins(builder.Configuration.GetSection("AllowedOrigin").Value!).AllowAnyHeader().AllowAnyMethod().AllowCredentials());
+    app.UseCors(c =>
+    c.WithOrigins(builder.Configuration.GetSection("AllowedOrigin").Value!)
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials());
     app.UseAuthentication();
     app.UseAuthorization();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
